9a43f4fa1e428ce84c9e14b2eb5fa430a10b239d
Phiec31691q <122201433+Phiec31691q@users.noreply.github.com>
2026-09-09 03:17:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1073,14 +1073,3 @@ process.on('warning', (w) => console.warn('[WARNING]', w.message));
 
 /* ============================ LOGIN ============================ */
 client.login(config.token).catch((e) => { console.error('[LOGIN] Hata:', e.message); process.exit(1); });
-// Express Web Server (Render Port İhtiyacı İçin)
-const app = express();
-const port = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-  res.send('Bot 7/24 aktif!');
-});
-
-app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda çalışıyor.`);
-});
